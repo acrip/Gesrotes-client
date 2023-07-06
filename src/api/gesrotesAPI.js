094fc734d0ee44bc5ed0554b73a1ddd6a29f12ef
acrip <jazapata@unicauca.edu.co>
2023-07-06 02:42:57 -0500

    Backend on tags running

--- src/api/gesrotesAPI.js
+++ src/api/gesrotesAPI.js
@@ -1,47 +1,79 @@
-import axios from 'axios'
+import axios from "axios";
 
-const gesrotesApi = axios.create(
-  {
-    baseURL: 'http://localhost:3000'
-  }
-)
+const gesrotesApi = axios.create({
+  baseURL: "http://localhost:8090",
+});
 
 export const getAsignaturas = async () => {
-  const res = await gesrotesApi.get('/asignaturas')
-  return res.data;
-}
+  const res = await gesrotesApi.get("/subjects");
+  return res.data.map((res) => ({
+    id: res.idSubject,
+    name: res.name,
+    carreraName: res.nameProgram,
+  }));
+};
 
 export const getAsignaturaById = async (asignaturaId) => {
-  const res = await gesrotesApi.get(`/asignaturas/${asignaturaId}`)
-  console.log(res.data);
-  return res.data;
-}
+  const res = await gesrotesApi.get(`/subjects?id=${asignaturaId}`);
+  return {
+    ...res.data,
+    id: res.data.idSubject,
+  };
+};
 
 export const getCarreraById = async (carreraId) => {
   const res = await gesrotesApi.get(`/carreras/${carreraId}`);
-  return res.data; 
+  return res.data;
 };
 
 export const getScenery = async () => {
-  const res = await gesrotesApi.get('/escenarios')
-  console.log(res.data);
-  return res.data;
-}
+  const res = await gesrotesApi.get("/scenaries");
+  return res.data.map((res) => ({
+    ...res,
+    id: res.idScenary,
+  }));
+};
 
 export const getEtiquetas = async () => {
-  const res = await gesrotesApi.get('/etiquetas')
-  console.log(res.data);
+  const res = await gesrotesApi.get("/scenary/tag");
+  console.log(res);
+  return res.data.map((res) => ({
+    ...res,
+    id: res.idTag,
+    nameScenery: res.nameScenary,
+  }));
+};
+
+export const postEtiqueta = async ({ nameTag, escenarioId }) => {
+  const res = await gesrotesApi.post("/scenary/tag/add", {
+    idScenary: escenarioId,
+    name: nameTag,
+  });
   return res.data;
+};
+
+export const getServicios = async () => {
+  const res = await gesrotesApi.get("/service");
+  console.log(res);
+  return res.data.map((res) => ({
+    id: res.idService,
+    name: res.name
+  }));
 }
 
-
-export const postEtiqueta = async (escenarioId, nameTag) => {
-  const res = await gesrotesApi.post('/etiquetas', {
-    escenarioId: escenarioId,
-    name: nameTag
-  })
-  console.log(res.data);
+export const deleteTag = async (id) => {
+  const res = await gesrotesApi.get(`/scenary/tag/delete?id=${id}`);
   return res.data;
-}
-  
+};
 
+export const postAsociateTag = async ({ idService, idTag }) => {
+  const res = await gesrotesApi.post(`/scenary/tagService/add?idTag=${idTag}&idService=${idService}`, {
+    idService: idService,
+    idTag: idTag,
+  });
+  return res.data;
+};
+
+export const api = {
+  deleteTag,
+};
